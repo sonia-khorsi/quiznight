987c138120e130fa571555aac4f260d372374db6
Réda YAHYAOUI <yahyaouir@yahoo.frgit config --global user.name Reda21-eng>
2025-02-12 14:37:46 +0100

    ajout lien inscription

--- index.php
+++ index.php
@@ -44,6 +44,7 @@ foreach ($allQuestions as $question) {
         <ul>
             <li><a href="index.php">Accueil</a></li>
             <li><a href="connexion.php">Se connecter</a></li>
+            <li><a href = "inscription.php"> S'inscrire </a></li>
         </ul>
     </nav>
 
