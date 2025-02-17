8cef815d101780addaa3d83c84eafc5bd44edc34
Réda YAHYAOUI <yahyaouir@yahoo.frgit config --global user.name Reda21-eng>
2025-02-17 11:24:28 +0100

    retrait hashage mot de passe

--- inscription.php
+++ inscription.php
@@ -8,9 +8,7 @@ if (isset($_SESSION["nom_utilisateur"])) {
     header('Location: index.php');
 }
 
-if ($_POST) {
-    $nom_utilisateur = $_POST["nom_utilisateur"];
-    $mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT); // Hash le mot de passe pour le stocker dans la base de données
+
 
     // Prépare la requête pour sélectionner les données de l'utilisateur, permettant de vérifier si le nom d'utilisateur est déjà utilisé ou non.
     $requete = $connexion->prepare("SELECT * FROM utilisateurs WHERE nom_utilisateur = :nom_utilisateur");
@@ -27,7 +25,7 @@ if ($_POST) {
         ]);
         header('Location: .php');
     }
-}
+
 
 ?>
 <!DOCTYPE html>
